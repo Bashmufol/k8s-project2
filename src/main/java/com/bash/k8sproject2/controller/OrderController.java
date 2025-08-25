420f3d22b37b88874b29154a180abc963b81ed88
Muhammed Bashir <bashmufol@gmail.com>
2025-08-25 23:36:12 +0100

    updated

--- src/main/java/com/bash/k8sproject2/controller/OrderController.java
+++ src/main/java/com/bash/k8sproject2/controller/OrderController.java
@@ -1,20 +1,32 @@
 package com.bash.k8sproject2.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bash.k8sproject2.entity.Order;
+import com.bash.k8sproject2.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
 
-    public Order addOrder(){
+    private final OrderService orderService;
 
+    @PostMapping("/add-order")
+    public Order addOrder(Order order){
+        return orderService.addOrder(order);
     }
 
+    @GetMapping("/get-orders")
     public List<Order> getOrders(){
-
+        return orderService.getOrders();
     }
-    public Order getOrderById(int id){}
+
+    @GetMapping("/get-orders/{id}")
+    public Order getOrderById(@PathVariable int id){
+        return orderService.getOrderById(id);
+    }
 }
--- src/main/java/com/bash/k8sproject2/service/OrderService.java
+++ src/main/java/com/bash/k8sproject2/service/OrderService.java
@@ -1,4 +1,28 @@
 package com.bash.k8sproject2.service;
 
+import com.bash.k8sproject2.entity.Order;
+import com.bash.k8sproject2.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository repo;
+
+    public Order addOrder(Order order) {
+        return repo.save(order);
+    }
+
+    public List<Order> getOrders() {
+        return repo.findAll();
+    }
+
+    public Order getOrderById(int id) {
+        return repo.findById(id).orElse(null);
+    }
 }
