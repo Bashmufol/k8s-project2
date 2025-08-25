37b90455b2547cfa085bb42649eb39c9e1e50320
Muhammed Bashir <bashmufol@gmail.com>
2025-08-25 23:20:21 +0100

    first commit

---
+++ src/main/java/com/bash/k8sproject2/K8sProject2Application.java
@@ -0,0 +1,13 @@
+package com.bash.k8sproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bash/k8sproject2/K8sProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.k8sproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
